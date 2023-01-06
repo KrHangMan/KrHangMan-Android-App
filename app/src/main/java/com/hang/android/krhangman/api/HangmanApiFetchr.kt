b6a3bc525d9034b5be2e031d82e37ce4a9a9d91f
acw <acwoo97@gmail.com>
2023-01-07 02:34:24 +0900

    style : app icon 변경

--- app/src/main/java/com/hang/android/krhangman/api/HangmanApiFetchr.kt
+++ app/src/main/java/com/hang/android/krhangman/api/HangmanApiFetchr.kt
@@ -1,13 +1,12 @@
 package com.hang.android.krhangman.api
 
-import android.content.Context
 import android.util.Log
-import com.hang.android.krhangman.db.Repository
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 private const val TAG="HangmanApiFetchr"
 private const val BASE_URL="http://ec2-13-125-198-195.ap-northeast-2.compute.amazonaws.com:80/"
@@ -16,7 +15,7 @@ class HangmanApiFetchr {
     private val hangmanApi:HangmanApi
 
     init {
-        val retrofit: Retrofit=Retrofit.Builder()
+        val retrofit: Retrofit =Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
@@ -24,9 +23,9 @@ class HangmanApiFetchr {
         hangmanApi=retrofit.create(HangmanApi::class.java)
     }
 
-    fun getRank(){
-        val test=hangmanApi.fetchContents()
-        test.enqueue(object:Callback<String> {
+   /* fun getRank(){
+        val test=hangmanApi.getRank()
+        test.enqueue(object: Callback<RankResponse> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
                 Log.d(TAG,"response:${response.body()} ")
             }
@@ -36,7 +35,8 @@ class HangmanApiFetchr {
             }
 
         })
-    }
+    }*/
+
     fun addUser(userName:String){
         val test=hangmanApi.addUser(userName)
 
@@ -57,26 +57,6 @@ class HangmanApiFetchr {
         })
     }
 
-   /* fun fetchPhotos(): LiveData<List<Rank>> {
-        val responseLiveData:MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<PhotoResponse> =flickrApi.fetchPhotos()
-
-        flickrRequest.enqueue(object : Callback<PhotoResponse>{
-            override fun onResponse(call: Call<PhotoResponse>, response: Response<PhotoResponse>) {
-                Log.d(TAG,"Response received")
-                val photoResponse=response.body()
-                var galleryItem=photoResponse?.galleryItem?.filterNot{it.url.isBlank()}
-                responseLiveData.value=galleryItem
-            }
-
-            override fun onFailure(call: Call<PhotoResponse>, t: Throwable) {
-                Log.d(TAG,"Failed to fetch photos",t)
-            }
-
-        })
-        return responseLiveData
-    }
-*/
 
     companion object{
         private var INSTANCE: HangmanApiFetchr?=null
