32976645020c11783089e18aba0004de0142066e
acw <acwoo97@gmail.com>
2023-01-13 01:00:22 +0900

    feat : 다크모드 테마 비활성화

--- app/src/main/java/com/hang/android/krhangman/KrHangmanApp.kt
+++ app/src/main/java/com/hang/android/krhangman/KrHangmanApp.kt
@@ -1,6 +1,7 @@
 package com.hang.android.krhangman
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.hang.android.krhangman.api.HangmanApiFetchr
 import com.hang.android.krhangman.db.DBRepository
 
@@ -8,6 +9,7 @@ class KrHangmanApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         DBRepository.initialize(this)
         HangmanApiFetchr.initialize()
     }
--- app/src/main/java/com/hang/android/krhangman/ui/LoginActivity.kt
+++ app/src/main/java/com/hang/android/krhangman/ui/LoginActivity.kt
@@ -5,9 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -68,10 +65,10 @@ class LoginActivity : AppCompatActivity() {
                     val editor = pref.edit()
                     editor.putBoolean(INITIAL_LAUNCH, true)
                     editor.apply()
-                    Log.d("SSSS",pref.getBoolean(INITIAL_LAUNCH,false).toString())
 
 
                     val intent = MainActivity.newIntent(this)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
                     startActivity(intent)
                 } else {
                     Toast.makeText(
