402090e0b4b22d7a7c530a778f6499b8608bd446
acw <acwoo97@gmail.com>
2023-01-14 02:07:21 +0900

    feat : 반응형 ui 재설계

--- app/src/main/java/com/hang/android/krhangman/api/Rank.kt
+++ app/src/main/java/com/hang/android/krhangman/api/Rank.kt
@@ -3,7 +3,7 @@ package com.hang.android.krhangman.api
 import com.google.gson.annotations.SerializedName
 
 data class Rank(
-    @SerializedName("rank") var rank: String,
+    @SerializedName("ranking") var rank: String,
     @SerializedName("username") var userName: String,
     @SerializedName("correct_cnt") var correct_cnt: String
 )
